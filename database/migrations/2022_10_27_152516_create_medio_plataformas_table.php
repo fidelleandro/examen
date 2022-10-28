1e63a7d8a9878fd0865106ed8c6b0f40273da8bb
fidel <fidelleandro@msn.com>
2022-10-28 10:33:24 -0500

    complete migrations

--- database/migrations/2022_10_27_152516_create_medio_plataformas_table.php
+++ database/migrations/2022_10_27_152516_create_medio_plataformas_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     {
         Schema::create('medio_plataformas', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('medio_id')->comment('');
-            $table->bigInteger('idPlataformaClasificacion')->comment('');
+            $table->bigInteger('medio_id')->unsigned()->comment('');
+            $table->bigInteger('idPlataformaClasificacion')->unsigned()->comment('');
             $table->string('valor')->comment('');
             $table->integer('alcance')->comment('');  
             $table->string('observacion')->comment('');  
@@ -31,18 +31,126 @@ return new class extends Migration
                        ->onCascade('delete');               
         });
         \App\Models\MedioPlataforma::insert([
-            ['id' => 3, 'medio_id' => 76, 'idPlataformaClasificacion' => 3,'valor' => '10','alcance' => 750000,'observacion' => '','created_at' => '2017-07-09 03:19:04','updated_at' => '2017-07-09 03:19:04','deleted_at' => NULL],
-            ['id' => 4, 'medio_id' => 76, 'idPlataformaClasificacion' =>2, 'valor' => '89.7','alcance' => 871428,'observacion' => '','created_at' => '2017-07-09 03:31:53','updated_at' => '2017-07-09 03:31:53','deleted_at' => NULL],
-            ['id' => 5, 'medio_id' => 22, 'idPlataformaClasificacion' =>6, 'valor' => 'www.elcomercio.pe','alcance' => 600000,'observacion' => 'El Comercio','created_at' => '2017-07-13 03:48:56','updated_at' => '2021-05-20 16:51:42','deleted_at' => NULL],
-            ['id' => 6, 'medio_id' => 22, 'idPlataformaClasificacion' =>10,'valor' => 'Página interior','alcance' => 321993,'observacion' => '','created_at' => '2017-07-13 03:48:56','updated_at' => '2017-09-10 02:52:23','deleted_at' => NULL],
-            ['id' => 54,'medio_id' => 64, 'idPlataformaClasificacion' =>2, 'valor' => '103.9FM','alcance' => 576000,'observacion' => '','created_at' => '2017-09-10 03:53:03','updated_at' => '2017-09-10 03:53:03','deleted_at' => NULL],
-            ['id' => 59,'medio_id' => 123,'idPlataformaClasificacion' =>1, 'valor' => '7','alcance' => 1000000,'observacion' => '','created_at' => '2017-09-10 04:26:45','updated_at' => '2017-09-10 04:26:45','deleted_at' => NULL],
-            ['id' => 60,'medio_id' => 123,'idPlataformaClasificacion' => 6,'valor' =>'http://www.tvperu.gob.pe/','alcance' => 100290,'observacion' => '','created_at' => '2017-09-10 04:26:45','updated_at' => '2017-09-10 04:26:45','deleted_at' => NULL],
-            ['id' => 78,'medio_id' => 64, 'idPlataformaClasificacion' => 6,'valor' => 'https://www.radionacional.com.','alcance' => 7500,'observacion' => '','created_at' => '2018-02-05 17:41:15','updated_at' => '2021-01-21 02:13:14','deleted_at' => NULL],
-            ['id' => 99,'medio_id' => 76, 'idPlataformaClasificacion' => 6,'valor' => 'http://rpp.pe/','alcance' => 443413,'observacion' => '','created_at' => '2018-03-12 21:50:29','updated_at' => '2018-03-12 21:50:29','deleted_at' => NULL],
-            ['id' => 193,'medio_id' => 64,'idPlataformaClasificacion' => 14,'valor' => 'https://www.facebook.com/queha','alcance' => 20000,'observacion' => 'QUÉ HACER','created_at' => '2018-10-04 17:37:23','updated_at' => '2019-08-16 17:50:14','deleted_at' => NULL],
-            ['id' => 514,'medio_id' => 76,'idPlataformaClasificacion' => 15,'valor' => 'https://www.youtube.com/channe','alcance' => 443413,'observacion' => '','created_at' => '2020-04-23 21:00:32','updated_at' => '2020-04-23 21:00:32','deleted_at' => NULL],
-            ['id' => 853,'medio_id' => 64,'idPlataformaClasificacion' => 14,'valor' => 'https://www.facebook.com/radio','alcance' => 268851,'observacion' => 'Nacional','created_at' => '2021-02-03 21:05:05','updated_at' => '2021-02-03 21:05:05','deleted_at' => NULL]          
+            ['id' => 3, 
+             'medio_id' => 76, 
+             'idPlataformaClasificacion' => 3,
+             'valor' => '10',
+             'alcance' => 750000,
+             'observacion' => '',
+             'created_at' => '2017-07-09 03:19:04',
+             'updated_at' => '2017-07-09 03:19:04',
+             'deleted_at' => NULL
+            ],
+            ['id' => 4, 
+             'medio_id' => 76, 
+             'idPlataformaClasificacion' => 2, 
+             'valor' => '89.7',
+             'alcance' => 871428,
+             'observacion' => '',
+             'created_at' => '2017-07-09 03:31:53',
+             'updated_at' => '2017-07-09 03:31:53',
+             'deleted_at' => NULL
+            ],
+            ['id' => 5, 
+             'medio_id' => 22, 
+             'idPlataformaClasificacion' => 6, 
+             'valor' => 'www.elcomercio.pe',
+             'alcance' => 600000,
+             'observacion' => 'El Comercio',
+             'created_at' => '2017-07-13 03:48:56',
+             'updated_at' => '2021-05-20 16:51:42',
+             'deleted_at' => NULL
+            ],
+            ['id' => 6, 
+             'medio_id' => 22, 
+             'idPlataformaClasificacion' => 10,
+             'valor' => 'Página interior',
+             'alcance' => 321993,
+             'observacion' => '',
+             'created_at' => '2017-07-13 03:48:56',
+             'updated_at' => '2017-09-10 02:52:23',
+             'deleted_at' => NULL
+            ],
+            ['id' => 54,
+             'medio_id' => 64, 
+             'idPlataformaClasificacion' => 2, 
+             'valor' => '103.9FM',
+             'alcance' => 576000,
+             'observacion' => '',
+             'created_at' => '2017-09-10 03:53:03',
+             'updated_at' => '2017-09-10 03:53:03',
+             'deleted_at' => NULL
+            ],
+            ['id' => 59,
+             'medio_id' => 123,
+             'idPlataformaClasificacion' => 1, 
+             'valor' => '7',
+             'alcance' => 1000000,
+             'observacion' => '',
+             'created_at' => '2017-09-10 04:26:45',
+             'updated_at' => '2017-09-10 04:26:45',
+             'deleted_at' => NULL
+            ],
+            ['id' => 60,
+             'medio_id' => 123,
+             'idPlataformaClasificacion' => 6,
+             'valor' =>'http://www.tvperu.gob.pe/',
+             'alcance' => 100290,
+             'observacion' => '',
+             'created_at' => '2017-09-10 04:26:45',
+             'updated_at' => '2017-09-10 04:26:45',
+             'deleted_at' => NULL
+            ],
+            ['id' => 78,
+             'medio_id' => 64, 
+             'idPlataformaClasificacion' => 6,
+             'valor' => 'https://www.radionacional.com.',
+             'alcance' => 7500,
+             'observacion' => '',
+             'created_at' => '2018-02-05 17:41:15',
+             'updated_at' => '2021-01-21 02:13:14',
+             'deleted_at' => NULL
+            ],
+            ['id' => 99,
+             'medio_id' => 76, 
+             'idPlataformaClasificacion' => 6,
+             'valor' => 'http://rpp.pe/',
+             'alcance' => 443413,
+             'observacion' => '',
+             'created_at' => '2018-03-12 21:50:29',
+             'updated_at' => '2018-03-12 21:50:29',
+             'deleted_at' => NULL
+            ],
+            ['id' => 193,
+             'medio_id' => 64,
+             'idPlataformaClasificacion' => 14,
+             'valor' => 'https://www.facebook.com/queha',
+             'alcance' => 20000,
+             'observacion' => 'QUÉ HACER',
+             'created_at' => '2018-10-04 17:37:23',
+             'updated_at' => '2019-08-16 17:50:14',
+             'deleted_at' => NULL
+            ],
+            ['id' => 514,
+             'medio_id' => 76,
+             'idPlataformaClasificacion' => 15,
+             'valor' => 'https://www.youtube.com/channe',
+             'alcance' => 443413,
+             'observacion' => '',
+             'created_at' => '2020-04-23 21:00:32',
+             'updated_at' => '2020-04-23 21:00:32',
+             'deleted_at' => NULL
+            ],
+            ['id' => 853,
+             'medio_id' => 64,
+             'idPlataformaClasificacion' => 14,
+             'valor' => 'https://www.facebook.com/radio',
+             'alcance' => 268851,
+             'observacion' => 'Nacional',
+             'created_at' => '2021-02-03 21:05:05',
+             'updated_at' => '2021-02-03 21:05:05',
+             'deleted_at' => NULL
+            ]          
         ]);
     }
 
--- database/migrations/2022_10_27_154138_create_personas_table.php
+++ database/migrations/2022_10_27_154138_create_personas_table.php
@@ -16,20 +16,68 @@ return new class extends Migration
             $table->id();
             $table->string('apellidos')->comment('');
             $table->string('nombres')->comment('');
-            $table->string('apodo')->comment('');
+            $table->string('apodo')->nullable()->comment('');
             $table->boolean('genero',1)->comment('');  
-            $table->date('fechaNacimiento')->comment(''); 
-            $table->string('profesion')->comment(''); 
-            $table->text('observacion')->comment(''); 
-            $table->boolean('famoso',1)->comment('');    
+            $table->date('fechaNacimiento')->nullable()->comment(''); 
+            $table->string('profesion')->nullable()->comment(''); 
+            $table->text('observacion')->nullable()->comment(''); 
+            $table->boolean('famoso',1)->nullable()->comment('');    
             $table->timestamps();
             $table->softDeletes();
         });
         \App\Models\Persona::insert([
-            ['id' => 306, 'apellidos' => 'Elías','nombres' => 'Betty','apodo' => NULL,'genero' => 0,'fechaNacimiento' => NULL,'profesion' => 'Productora','observacion' => NULL,'famoso' => NULL,'created_at' => '2017-06-06 22:59:36','updated_at' => '2021-08-06 20:22:35','deleted_at' => NULL],
-            ['id' => 330, 'apellidos' => 'Acuña Villalobos','nombres' => 'Pedro','apodo' => NULL,'genero' => 1,'fechaNacimiento' => NULL,'profesion' => 'Editor General de Televisión','observacion' => NULL,'famoso' => NULL,'created_at' => '2017-06-17 22:53:24','updated_at' => '2021-03-15 18:48:56','deleted_at' => NULL],
-            ['id' => 1102,'apellidos' => 'Coya ','nombres' => 'Hugo','apodo' => '','genero' => 1,'fechaNacimiento' => NULL,'profesion' => '','observacion' => '','famoso' => 1,'created_at' => '2019-05-17 14:33:52','updated_at' => '2019-05-17 14:33:52','deleted_at' => NULL],
-            ['id' => 1583,'apellidos' => 'Garcia ','nombres' => 'Brenda','apodo' => '','genero' => 0,'fechaNacimiento' => NULL,'profesion' => '','observacion' => '','famoso' => NULL,'created_at' => '2020-04-23 14:45:57','updated_at' => '2020-04-23 14:45:57','deleted_at' => NULL]
+            ['id' => 306, 
+             'apellidos' => 'Elías',
+             'nombres' => 'Betty',
+             'apodo' => NULL,
+             'genero' => 0,
+             'fechaNacimiento' => NULL,
+             'profesion' => 'Productora',
+             'observacion' => NULL,
+             'famoso' => NULL,
+             'created_at' => '2017-06-06 22:59:36',
+             'updated_at' => '2021-08-06 20:22:35',
+             'deleted_at' => NULL
+            ],
+            ['id' => 330, 
+             'apellidos' => 'Acuña Villalobos',
+             'nombres' => 'Pedro',
+             'apodo' => NULL,
+             'genero' => 1,
+             'fechaNacimiento' => NULL,
+             'profesion' => 'Editor General de Televisión',
+             'observacion' => NULL,
+             'famoso' => NULL,
+             'created_at' => '2017-06-17 22:53:24',
+             'updated_at' => '2021-03-15 18:48:56',
+             'deleted_at' => NULL
+            ],
+            ['id' => 1102,
+             'apellidos' => 'Coya ',
+             'nombres' => 'Hugo',
+             'apodo' => '',
+             'genero' => 1,
+             'fechaNacimiento' => NULL,
+             'profesion' => '',
+             'observacion' => '',
+             'famoso' => 1,
+             'created_at' => '2019-05-17 14:33:52',
+             'updated_at' => '2019-05-17 14:33:52',
+             'deleted_at' => NULL
+            ],
+            ['id' => 1583,
+             'apellidos' => 'Garcia ',
+             'nombres' => 'Brenda',
+             'apodo' => '',
+             'genero' => 0,
+             'fechaNacimiento' => NULL,
+             'profesion' => '',
+             'observacion' => '',
+             'famoso' => NULL,
+             'created_at' => '2020-04-23 14:45:57',
+             'updated_at' => '2020-04-23 14:45:57',
+             'deleted_at' => NULL
+             ]
         ]);    
     }
 
